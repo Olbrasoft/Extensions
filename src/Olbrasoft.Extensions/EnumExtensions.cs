8da06cfaf168a23376731c18283894821587ccb1
MatrixStore <matrixstore@email.cz>
2020-03-16 00:29:35 +0100

    ini

---
+++ src/Olbrasoft.Extensions/EnumExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+
+namespace Olbrasoft.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDescription(this Enum value)
+        {
+            var result = value
+                             .GetType()
+                             .GetMember(value.ToString())
+                             .FirstOrDefault()
+                             ?.GetCustomAttribute<DescriptionAttribute>()
+                             ?.Description ?? value.ToString();
+
+            return result;
+        }
+    }
+}
