d4a13b24c12e7df9e30063c2d8f396575a816d78
MatrixStore <matrixstore@email.cz>
2020-04-01 04:20:49 +0200

    add at word

--- src/Olbrasoft.Extensions/StringExtensions.cs
+++ src/Olbrasoft.Extensions/StringExtensions.cs
@@ -1,11 +1,24 @@
-﻿namespace Olbrasoft.Extensions
+﻿using System;
+
+namespace Olbrasoft.Extensions
 {
     public static class StringExtensions
     {
-        public static string Truncate(this string value, int maxLength)
+        public static string Truncate(this string value, int maxLength, bool atWord = false)
         {
             if (string.IsNullOrEmpty(value)) return value;
+
+            if (atWord) return TruncateAtWord(value, maxLength);
+
             return value.Length <= maxLength ? value : value.Substring(0, maxLength);
+        }
+
+        private static string TruncateAtWord(this string value, int length)
+        {
+            if (value.Length < length) return value;
+
+            int iNextSpace = value.LastIndexOf(" ", length, StringComparison.Ordinal);
+            return string.Format("{0}", value.Substring(0, (iNextSpace > 0) ? iNextSpace : length).Trim());
         }
     }
 }
